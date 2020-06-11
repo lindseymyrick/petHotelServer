e9e407a95d2928f05271258ecc09b20c516227ca
Heather Stoffels <Umdstarlet@gmail.com>
2020-06-11 13:43:49 -0500

    working on post

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import psycopg2
 
 
 
-@app.route('/pets', methods=['GET'])
+@app.route('/pets', methods=['GET', 'POST'])
 def handlePets():
     if request.method == "GET":
         # make database call
@@ -15,8 +15,20 @@ def handlePets():
                 "owners"."name" FROM "pets" JOIN "owners" ON "owner_id" = "owners"."id";'
             cursor.execute(queryText)
             pet_records = cursor.fetchall()
-        print("Get successful")
-        for row in pet_records:
-            print("id=", row[0])
-    except (Exception, psycopg2.Error) as error :
-        print ("Error while fetching data from PostgreSQL", error)
+            print("Get successful")
+            return pet_records     
+        except (Exception, psycopg2.Error) as error :
+            print ("Error while fetching data from PostgreSQL", error)
+        finally:
+    #closing database connection.
+            if(connection):
+                cursor.close()
+                connection.close()
+                print("PostgreSQL connection is closed") 
+
+    elif request.method == "POST":           
+        try:
+            connection = psycopg2.connect(host="127.0.0.1", port="5432", database="pet_hotel")
+            cursor = conn.cursor()
+            queryText = 'INSERT INTO pets ("name", "color", "breed", "owner_id") VALUES ('%s', '%s', '%s', '%d');'
+            pet_to_insert = 
