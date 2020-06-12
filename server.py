8bad73eb78a4195a5e8836d980c88a8572f7130c
Shaokee Moua <m.shaokee@gmail.com>
2020-06-12 09:53:17 -0500

    adjusted query for dashboard

--- server.py
+++ server.py
@@ -5,42 +5,69 @@ import psycopg2
 
 app = Flask(__name__)
 
+# to get server running.
+# export FLASK_APP=server.py
+# or this: export FLASK_ENV=development
 
 
 @app.route('/pets', methods=['GET', 'POST'])
 def handlePets():
     if request.method == "GET":
         # make database call
-        # opens up connection 
-        conn = psycopg2.connect(host="127.0.0.1", port="5432", database="pet_hotel")
+        # opens up connection
+        conn = psycopg2.connect(
+            host="127.0.0.1", port="5432", database="pet_hotel")
         try:
             cursor = conn.cursor()
-            queryText = 'SELECT "owners"."name" as "owner", "pets"."name" as "pet", "pets"."breed", "pets"."color", "pets"."checked_in", "owners"."id" as "owner_id", "pets"."id" as "pet_id" FROM "pets" JOIN "owners" ON "owner_id" = "owners"."id";'
-        #execute sql query
+            queryText = '''
+            SELECT "owners"."name" as "owner", "pets"."name" as "pet", "pets"."breed", "pets"."color", "pets"."checked_in", "owners"."id" as "owner_id", "pets"."id" as "pet_id"  
+            FROM "pets"
+            JOIN "owners" 
+            ON "owner_id" = "owners"."id"
+            ;'''
+        # execute sql query
             cursor.execute(queryText)
-        #data from databse
+        # data from databse
             pet_records = cursor.fetchall()
             print("Get successful")
-         #equivalent to res.send hopefully?
-            return jsonify(pet_records)     
-        except (Exception, psycopg2.Error) as error :
-            print ("Error while fetching data from PostgreSQL", error)
+         # equivalent to res.send hopefully?
+            return jsonify(pet_records)
+        except (Exception, psycopg2.Error) as error:
+            print("Error while fetching data from PostgreSQL", error)
         finally:
-    #closing database connection.
+            # closing database connection.
             if(conn):
                 cursor.close()
                 conn.close()
-                print("PostgreSQL connection is closed") 
+                print("PostgreSQL connection is closed")
 
-    elif request.method == "POST":   
-        connection = psycopg2.connect(host="127.0.0.1", port="5432", database="pet_hotel")        
-        try:
-            # connection = psycopg2.connect(host="127.0.0.1", port="5432", database="pet_hotel")
-            cursor = conn.cursor()
-            #request.get_json = req.body
-            name = request.get_json['name']
-            color = request.get_json['color']
-            breed = request.get_json['breed']
-            owner_id = request.get_json['owner_id']
-            queryText = 'INSERT INTO pets ("name", "color", "breed", "owner_id") VALUES (%s %s %s %d);'
-            cursor.execute(queryText, (name, color, breed, owner_id))
+    # elif request.method == "POST":
+    #     # open database post
+    #     conn = psycopg2.connect(
+    #         host="127.0.0.1", port="5432", database="pet_hotel")
+    #     try:
+    #         # connection = psycopg2.connect(host="127.0.0.1", port="5432", database="pet_hotel")
+    #         cursor = conn.cursor()
+    #         #request.get_json = req.body
+    #         name = 'hello' #request.get_json['name']
+    #         color = 'rainbow' #request.get_json['color']
+    #         breed = 'rare' #request.get_json['breed']
+    #         owner_id = 1 #request.get_json['owner_id']
+    #         queryText = 'INSERT INTO pets ("name", "color", "breed", "owner_id") VALUES (%s, %s, %s, %d);'
+    #         insertData = (name, color, breed, owner_id)
+    #         #execute query (currently hardcoded)
+    #         cursor.execute(queryText, insertData)
+    #         #iffy about next stuff
+    #         conn.commit()
+    #         count = cursor.rowcount
+    #         print(count, 'HEY IF IT POSTED THIS SHOWS')
+    #     except(Exception, psycopg2.Error) as error :
+    #         if(conn):
+    #             print('FAIL', error)
+    #     finally: 
+    #         #closing
+    #         if(conn):
+    #             cursor.close()
+    #             conn.close()
+    #             print('POSTGRESQL CLOSED')
+
